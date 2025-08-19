c137340d1ea8cf7988d4f3c6670a97518e3c3240
Mahmuda Akhter <mahmudaakhter2468@gmail.com>
2025-08-19 17:09:46 +0600

    Fix: Correct casing for FlashSales component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Banner from "../components/Banner";
-import FlashSales from "../components/Flashsales";
+
 import BrowseCategory from "../components/BrowseCategory";
 import BestSelling from "../components/BestSelling";
 import Offer from "../components/Offer";
 import NewArrival from "../components/NewArrival";
 import CustomerService from "../components/CustomerService";
 import ExplourProducts from "../components/ExplourProducts";
+import FlashSales from "../components/FlashSales";
 
 
 
